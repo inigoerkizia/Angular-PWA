055f2af7cdc93b5e8ec6b987c612e0877dbc3b4c
inigoerkizia <2g.erquicia.inigo@gmail.com>
2024-06-26 12:36:12 +0200

    tercer día

--- segundo-proyecto/src/app/app.config.ts
+++ segundo-proyecto/src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
- 
+
 import { routes } from './app.routes';
-import { ContactoServiceInterface } from './services/contacto.service.interface';
 import { ContactoService } from './services/contacto.service';
- 
+import { ContactoServiceInterface } from './services/contacto.service.interface';
+import { provideHttpClient } from '@angular/common/http';
+import { ContactoServiceHttp } from './services/contacto.service.http';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    {provide: ContactoServiceInterface, useClass: ContactoService}]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide : ContactoServiceInterface, useClass : ContactoServiceHttp}
+  ]
 };
--- segundo-proyecto/src/app/components/listado/listado.component.ts
+++ segundo-proyecto/src/app/components/listado/listado.component.ts
@@ -1,9 +1,10 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ContactoModel } from '../../models/contacto.model';
 import { CommonModule } from '@angular/common';
 import { ContactoServiceInterface } from '../../services/contacto.service.interface';
- 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-listado',
   standalone: true,
@@ -12,19 +13,44 @@ import { ContactoServiceInterface } from '../../services/contacto.service.interf
   styleUrl: './listado.component.css'
 })
 export class ListadoComponent implements OnInit {
- 
+
   contactos: ContactoModel[] = [];
- 
+
   constructor(private service: ContactoServiceInterface) {
- 
+
   }
- 
-  //Se ejecuta cuando el componente ya se mostro
-  ngOnInit(){
+
+  recuperarContactosDelBackend(){
     this.service.getContactos().subscribe({
       next : (lista : ContactoModel[]) =>{
         this.contactos = lista;
       }
+    })
+  }
+
+  //Se ejecuta cuando el componente ya se mostro
+  ngOnInit(){
+    this.recuperarContactosDelBackend();
+  }
+
+  eliminar(id:number){
+
+    Swal.fire({
+      title: '¿Está seguro?',
+      text: 'Está a punto de eliminar un contacto',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.service.eliminar(id).subscribe({
+          next : () => {
+            this.recuperarContactosDelBackend();
+          }
+        })
+      }
+    
     })
   }
 }
